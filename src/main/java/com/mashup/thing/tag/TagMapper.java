66b6622624d0e02f67a33b22b1356a715185c79d
korea8378 <korea8378@naver.com>
2019-07-24 23:00:25 +0900

    [#6][ADD] 태그 리스트 조회 (#20)

---
+++ src/main/java/com/mashup/thing/tag/TagMapper.java
@@ -0,0 +1,31 @@
+package com.mashup.thing.tag;
+
+import com.mashup.thing.tag.domain.Tag;
+import com.mashup.thing.tag.domain.TagType;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TagMapper {
+    public ResTagsDto toResTagsDto(List<Tag> tags) {
+        List<ResTagsDto.ResTagDto> tagDtos = new ArrayList<>();
+
+        for (TagType tagType : TagType.values()) {
+            tagDtos.add(ResTagsDto.ResTagDto.builder()
+                    .category(tagType.getTagType())
+                    .list(new ArrayList<>())
+                    .build());
+        }
+
+        for (Tag tag : tags) {
+            tagDtos.get(tag.getMainTagIndex())
+                    .addSubTag(tag.getSubTag());
+        }
+
+        return ResTagsDto.builder()
+                .tags(tagDtos)
+                .build();
+    }
+}
