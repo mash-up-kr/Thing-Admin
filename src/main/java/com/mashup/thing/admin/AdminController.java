032f9c78f80491161100ca1aacfac8cbe191430b
korea8378 <korea8378@naver.com>
2019-08-16 20:44:15 +0900

    [#26][ADD] CORS를 위한 @CrossOrigin 추가 (#27)

--- src/main/java/com/mashup/thing/admin/AdminController.java
+++ src/main/java/com/mashup/thing/admin/AdminController.java
@@ -6,6 +6,7 @@ import com.mashup.thing.exception.RequestNullFieldException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+//ToDO 추후 배포 했을때의 도메인주소로 변경해야함
+@CrossOrigin(origins = "*")
 public class AdminController {
 
     private final AdminService adminService;
